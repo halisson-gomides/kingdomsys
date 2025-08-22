23934836bbfb2da739b6d2e488413ac5f8d4a31d
Halisson Gomides <halisson.gomides@gmail.com>
2025-08-22 15:41:22 +0000

    ver 1.0

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
-from mailersend import emails
+from mailersend import MailerSendClient, EmailBuilder
 import uvicorn
+from datetime import datetime
 
 load_dotenv()
 
@@ -18,43 +19,35 @@ class ContactForm(BaseModel):
 
 @app.post("/api/send_email")
 async def send_email(form: ContactForm):
-    mailer = emails.NewEmail()
+    ms = MailerSendClient()
 
-    mail_body = {}
+    email = (EmailBuilder()
+         .from_email(os.getenv("FROM_EMAIL"), form.nome)
+         .to_many([{"email": os.getenv("TO_EMAIL"), "name": "Administrador"}])
+         .template("0r83ql3opy0lzw1j")
+         .personalize_many([{
+             "email": os.getenv("TO_EMAIL"),
+             "data": {
+                 "user":{
+                     "mail":form.email,
+                     "phone":form.telefone,
+                     "message":form.mensagem,
+                     "subject":form.subject
+                 },
+                 "comment":{
+                     "date":datetime.now().strftime("%d/%m/%Y %H:%M")
+                 },
+                 "account_name":"KingdomSys"
+             }
+         }])
+         .build())
 
-    mail_from = {
-        "name": form.nome,
-        "email": os.getenv("FROM_EMAIL"),
-    }
-
-    recipients = [
-        {
-            "name": "Recipient",
-            "email": os.getenv("TO_EMAIL"),
-        }
-    ]
-
-    mailer.set_mail_from(mail_from, mail_body)
-    mailer.set_mail_to(recipients, mail_body)
-    mailer.set_subject(form.subject, mail_body)
-
-    html_content = f"""
-    <h2>KingdomSys</h2>
-    <h3>Nova mensagem do formulário de contato</h3>
-    <p><strong>Nome:</strong> {form.nome}</p>
-    <p><strong>Email:</strong> {form.email}</p>
-    <p><strong>Telefone:</strong> {form.telefone}</p>
-    <p><strong>Assunto:</strong> {form.subject}</p>
-    <p><strong>Mensagem:</strong></p>
-    <p>{form.mensagem}</p>
-    """
-    mailer.set_html_content(html_content, mail_body)
 
     try:
-        mailer.send(mail_body)
+        response = ms.emails.send(email)
         return {"message": "Email sent successfully"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=e.__repr__())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
